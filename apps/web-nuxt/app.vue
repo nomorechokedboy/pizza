a2ac4ac7c8df217a22e964199bd4b7a35b595580
nomorechokedboy <haiduongit@protonmail.com>
2023-01-30 13:00:11 +0700

    feat(admin-nuxt): initialize admin nuxt and add tailwindcss, vitest config

--- apps/web-nuxt/app.vue
+++ apps/web-nuxt/app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtWelcome />
+		<pizza-button class="lmao">Test 1</pizza-button>
+		<div class="flex">
+			<p class="!text-cyan-500">Test tailwindcss</p>
+		</div>
+	</div>
 </template>
---
+++ packages/ui-vue/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+	<button class="bg-cyan-500 text-cyan-500">
+		<slot></slot>
+	</button>
+</template>
