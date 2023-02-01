5ef51bf954831328b7660bc334a2c94b5c111937
nomorechokedboy <haiduongit@protonmail.com>
2023-02-01 11:12:48 +0700

    feat(api/category): implement insert category usecase

--- apps/api/src/category/domain/usecases/insert.go
+++ apps/api/src/category/domain/usecases/insert.go
@@ -1,11 +1,13 @@
 package usecases
 
-import "api/src/category/domain"
+import (
+	"api/src/category/domain"
+)
 
 type InsertCategoryUseCase struct {
 	Repo CategoryRepository
 }
 
 func (useCase *InsertCategoryUseCase) Execute(req *domain.WriteCategoryBody) (*domain.Category, error) {
-	return nil, nil
+	return useCase.Repo.Insert(req)
 }
--- apps/api/src/category/repository/in_memory.go
+++ apps/api/src/category/repository/in_memory.go
@@ -1,6 +1,9 @@
 package repository
 
-import "api/src/category/domain"
+import (
+	"api/src/category/domain"
+	"errors"
+)
 
 type CategoryInMemoryRepo struct {
 	Data  []domain.Category
@@ -8,5 +11,19 @@ type CategoryInMemoryRepo struct {
 }
 
 func (repo *CategoryInMemoryRepo) Insert(req *domain.WriteCategoryBody) (*domain.Category, error) {
-	return nil, nil
+	if repo.IsErr {
+		return nil, errors.New("unknown error")
+	}
+
+	for _, product := range repo.Data {
+		if product.Name == req.Name {
+			return nil, errors.New("resource exist")
+		}
+	}
+
+	Id := len(repo.Data) + 1
+	newCategory := domain.Category{Id: Id, Description: req.Description, Name: req.Name}
+	repo.Data = append(repo.Data, newCategory)
+
+	return &newCategory, nil
 }
