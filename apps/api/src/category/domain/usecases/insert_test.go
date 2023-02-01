53b1ce766f3407659717d1db79b833363aa58a97
nomorechokedboy <haiduongit@protonmail.com>
2023-02-01 11:05:22 +0700

    test(api/category): add insert category test cases

--- apps/api/src/category/domain/usecases/insert_test.go
+++ apps/api/src/category/domain/usecases/insert_test.go
@@ -1,15 +1,42 @@
 package usecases_test
 
-import "testing"
+import (
+	"api/src/category/domain"
+	"api/src/category/domain/usecases"
+	"api/src/category/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var categoryRepo = repository.CategoryInMemoryRepo{Data: make([]domain.Category, 0), IsErr: false}
+var insertUseCase = usecases.InsertCategoryUseCase{Repo: &categoryRepo}
+var req = domain.WriteCategoryBody{Name: "Comedy", Description: "Funny stuffs"}
 
 func TestInsertCategoryWithUnknownError(t *testing.T) {
+	assert := assert.New(t)
+	categoryRepo.IsErr = true
+	category, err := insertUseCase.Execute(&req)
 
+	assert.EqualError(err, "unknown error")
+	assert.Nil(category)
+	categoryRepo.IsErr = false
 }
 
 func TestInsertCategoryWithDuplicateError(t *testing.T) {
+	assert := assert.New(t)
+	category, err := insertUseCase.Execute(&req)
 
+	assert.EqualError(err, "resource exist")
+	assert.Nil(category)
 }
 
 func TestInsertCategoryHappyCase(t *testing.T) {
+	assert := assert.New(t)
+	category, err := insertUseCase.Execute(&req)
 
+	assert.Nil(err)
+	assert.NotNil(category)
+	assert.Equal(req.Description, category.Description)
+	assert.Equal(req.Name, category.Name)
 }
