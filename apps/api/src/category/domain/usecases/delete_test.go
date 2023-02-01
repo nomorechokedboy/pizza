ca13a881962b8cd1abf56030a19f8bb10417f70b
nomorechokedboy <haiduongit@protonmail.com>
2023-02-01 11:48:15 +0700

    test(api/category): add test cases for delete usecase

--- apps/api/src/category/domain/usecases/delete_test.go
+++ apps/api/src/category/domain/usecases/delete_test.go
@@ -1,15 +1,42 @@
 package usecases_test
 
-import "testing"
+import (
+	"api/src/category/domain"
+	"api/src/category/domain/usecases"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var deleteUsecase = usecases.DeleteCategoryUseCase{Repo: &categoryRepo}
 
 func TestDeleteCategoryUseCaseWithUnknownError(t *testing.T) {
+	assert := assert.New(t)
+	categoryRepo.IsErr = true
+	id := 1
+	deletedCategory, err := deleteUsecase.Execute(&id)
 
+	assert.Nil(deletedCategory)
+	assert.EqualError(err, "unknown error")
+	categoryRepo.IsErr = false
 }
 
 func TestDeleteCategoryUseCaseWithNotFoundError(t *testing.T) {
+	assert := assert.New(t)
+	id := 10
+	deletedCategory, err := deleteUsecase.Execute(&id)
 
+	assert.Nil(deletedCategory)
+	assert.EqualError(err, "not found")
 }
 
 func TestDeleteCategoryHappyCase(t *testing.T) {
+	assert := assert.New(t)
+	id := 5
+	categoryRepo.Data = append(categoryRepo.Data, domain.Category{Name: "Shounen", Description: "Blah blah, bloh bloh description", Id: 5})
+	deletedCategory, err := deleteUsecase.Execute(&id)
 
+	assert.Nil(err)
+	assert.Equal(id, deletedCategory.Id)
+	assert.Equal(len(categoryRepo.Data), 0)
 }
