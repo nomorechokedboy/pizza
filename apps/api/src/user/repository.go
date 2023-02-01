311f54c95b514eefa961aada49b5337005954565
kientin <tinhau1204@gmail.com>
2023-02-01 14:20:51 +0700

    feat(api/user): edit testcase correct database

--- apps/api/src/user/repository.go
+++ apps/api/src/user/repository.go
@@ -17,13 +17,13 @@ func (repo *UserInMemoryRepo) Insert(req *domain.CreateUserReq) (*domain.User, e
 	}
 
 	for _, user := range repo.UserList {
-		if user.UserName == req.UserName {
+		if user.Email == req.Email {
 			return nil, errors.New("user email already exist")
 		}
 	}
 
 	Id := len(repo.UserList) + 1
-	newUser := domain.User{Id: int32(Id), CreatedAt: time.Now(), UpdatedAt: time.Now(), FirstName: req.FirstName, LastName: req.LastName, UserName: req.UserName, Email: req.Email, Password: req.Password}
+	newUser := domain.User{Id: int32(Id), CreatedAt: time.Now(), UpdatedAt: time.Now(), FullName: req.FullName, Email: req.Email, Password: req.Password, BirthDate: req.BirthDate, Gender: req.Gender, PhoneNumber: req.PhoneNumber, Identifier: req.Identifier}
 	repo.UserList = append(repo.UserList, newUser)
 	return &newUser, nil
 }
