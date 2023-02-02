6a56b252eca13f3a8378fb6b0b09c8ada13a363e
kientin <tinhau1204@gmail.com>
2023-02-02 21:09:50 +0700

    feat(api/user): add Create User method

--- apps/api/src/user/infrastructure.go
+++ apps/api/src/user/infrastructure.go
@@ -1,1 +1,26 @@
 package user
+
+import (
+	"api/src/user/domain"
+	"api/src/user/domain/usecases"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// CreateUser godoc
+
+func CreateUser(ctx *fiber.Ctx) error {
+	req := domain.CreateUserReq{}
+
+	if err := ctx.BodyParser(&req); err != nil {
+		return ctx.Status(500).JSON(nil)
+	}
+
+	useCase := ctx.Locals("CreateUserUseCase").(usecases.CreateUserUseCase)
+	user, err := useCase.Execute(&req)
+	if err != nil {
+		return ctx.Status(409).JSON(nil)
+	}
+
+	return ctx.Status(201).JSON(user)
+}
