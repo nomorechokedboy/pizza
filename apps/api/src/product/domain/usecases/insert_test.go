b5f455c698a2462afb6a4f71eebce037ae9dab3a
nomorechokedboy <haiduongit@protonmail.com>
2023-02-03 10:24:46 +0700

    refactor(api/product): refactor insert test

--- apps/api/src/product/domain/usecases/insert_test.go
+++ apps/api/src/product/domain/usecases/insert_test.go
@@ -1,12 +1,14 @@
 package usecases_test
 
 import (
+	category "api/src/category/domain"
+	inventory "api/src/inventory/domain"
 	"api/src/product/domain"
 	"api/src/product/domain/usecases"
 	"api/src/product/repository"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -16,43 +18,45 @@ type InsertProductTestSuite struct {
 	Repo    repository.ProductInMemoryRepo
 }
 
-func TestCreateProductUseCaseWithUnknownError(t *testing.T) {
-	assert := assert.New(t)
-	productMemRepo := repository.ProductInMemoryRepo{DataStore: make([]domain.Product, 0), IsErr: false}
-	productMemRepo.IsErr = true
-	createProductUseCase := usecases.InsertProductUseCase{Repo: &productMemRepo}
-	req := domain.ProductReq{Description: "Test Description", Name: "Should ok", SKU: "Success", Price: 10000}
-	product, err := createProductUseCase.Execute(&req)
-
-	assert.EqualError(err, "unknown error")
-	assert.Nil(product)
+func (s *InsertProductTestSuite) SetupTest() {
+	s.Repo = repository.ProductInMemoryRepo{DataStore: make([]domain.Product, 0), IsErr: false}
+	s.UseCase = usecases.InsertProductUseCase{Repo: &s.Repo}
 }
 
-func TestCreateProductUseCaseWithDuplicateError(t *testing.T) {
-	assert := assert.New(t)
-	productMemRepo := repository.ProductInMemoryRepo{DataStore: make([]domain.Product, 0), IsErr: false}
-	_, err := productMemRepo.Insert(&domain.ProductReq{Description: "Another description", Name: "Lmao", SKU: "The duplicate SKU", Price: 101010})
-	assert.Nil(err)
-	createProductUseCase := usecases.InsertProductUseCase{Repo: &productMemRepo}
+func (s *InsertProductTestSuite) TearDownTest() {
+	s.UseCase.Repo = &repository.ProductInMemoryRepo{DataStore: make([]domain.Product, 0), IsErr: false}
+}
 
+func (s *InsertProductTestSuite) TestInsertWithErrors() {
+	s.Repo.IsErr = true
+	req := domain.ProductReq{Description: "Test Description", Name: "Should ok", SKU: "Success", Price: 10000}
+	product, err := s.UseCase.Execute(&req)
+
+	s.Assertions.EqualError(err, "unknown error")
+	s.Assertions.Nil(product)
+}
+
+func (s *InsertProductTestSuite) TestInsertWithDuplicateError() {
 	req := domain.ProductReq{Description: "Test Description", Name: "Should ok", SKU: "The duplicate SKU", Price: 10000}
-	product, err := createProductUseCase.Execute(&req)
+	s.Repo.DataStore = append(s.Repo.DataStore, domain.Product{Id: 1, Description: "Another description", Name: "Lmao", SKU: "The duplicate SKU", Price: 101010, CreatedAt: time.Now(), UpdatedAt: time.Now(), Slug: "lmao", Category: category.Category{}, Inventory: inventory.Inventory{}})
+	product, err := s.UseCase.Execute(&req)
 
-	assert.EqualError(err, "resource exist")
-	assert.Nil(product)
+	s.Assertions.EqualError(err, "resource exist")
+	s.Assertions.Nil(product)
 }
 
-func TestCreateProductUseCaseHappyCase(t *testing.T) {
-	assert := assert.New(t)
-	productMemRepo := repository.ProductInMemoryRepo{DataStore: make([]domain.Product, 0), IsErr: false}
-	createProductUseCase := usecases.InsertProductUseCase{Repo: &productMemRepo}
+func (s *InsertProductTestSuite) TestInsertHappyCase() {
 	req := domain.ProductReq{Description: "Test Description", Name: "Should ok", SKU: "Success", Price: 10000}
-	product, err := createProductUseCase.Execute(&req)
+	product, err := s.UseCase.Execute(&req)
 
-	assert.Nil(err)
-	assert.NotNil(product)
-	assert.Equal(product.Description, req.Description)
-	assert.Equal(product.Name, req.Name)
-	assert.Equal(product.Price, req.Price)
-	assert.Equal(product.SKU, req.SKU)
+	s.Assertions.Nil(err)
+	s.Assertions.NotNil(product)
+	s.Assertions.Equal(product.Description, req.Description)
+	s.Assertions.Equal(product.Name, req.Name)
+	s.Assertions.Equal(product.Price, req.Price)
+	s.Assertions.Equal(product.SKU, req.SKU)
+}
+
+func TestInsertProductTestSuite(t *testing.T) {
+	suite.Run(t, new(InsertProductTestSuite))
 }
