f7f3ac8c29b8d0d54a86fd0fb459711f59a7c1ce
nomorechokedboy <haiduongit@protonmail.com>
2023-02-10 15:16:51 +0700

    feat(api/category): proper error handle for gorm

--- apps/api/src/category/repository/gorm.go
+++ apps/api/src/category/repository/gorm.go
@@ -5,6 +5,7 @@ import (
 	apiUtils "api/src/utils"
 	"errors"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -16,7 +17,11 @@ type CategoryGormRepo struct {
 func (repo *CategoryGormRepo) Insert(req *domain.WriteCategoryBody) (*domain.Category, error) {
 	category := domain.Category{Name: req.Name, Description: &req.Description}
 	if result := repo.DB.Create(&category); result.Error != nil {
-		return nil, result.Error
+		if result.Error.(*pgconn.PgError).Code == "23505" {
+			return nil, nil
+		}
+
+		return nil, errors.New("unknown error")
 	}
 
 	returning := domain.Category{ID: category.ID, Name: category.Name, Description: category.Description}
@@ -28,7 +33,7 @@ func (repo *CategoryGormRepo) Update(id *int, req *domain.WriteCategoryBody) (*d
 	updateReq := domain.Category{Name: req.Name, Description: &req.Description}
 	result := repo.DB.Model(&domain.Category{}).Where("id = ?", id).Updates(&updateReq)
 	if result.Error != nil {
-		return nil, result.Error
+		return nil, errors.New("unknown error")
 	}
 
 	if result.RowsAffected == 0 {
@@ -43,7 +48,7 @@ func (repo *CategoryGormRepo) Delete(req *int) (*domain.Category, error) {
 	deleteReq := domain.Category{ID: uint(*req)}
 	result := repo.DB.Clauses(clause.Returning{}).Delete(&deleteReq)
 	if result.Error != nil {
-		return nil, result.Error
+		return nil, errors.New("unknown error")
 	}
 	if result.RowsAffected == 0 {
 		return nil, errors.New("not found")
@@ -58,7 +63,11 @@ func (repo *CategoryGormRepo) FindOne(id *int) (*domain.Category, error) {
 	dbCategory := domain.Category{ID: uint(*id)}
 
 	if result := repo.DB.First(&dbCategory); result.Error != nil {
-		return nil, result.Error
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, errors.New("not found")
+		}
+
+		return nil, errors.New("unknown error")
 	}
 
 	category := domain.Category{ID: dbCategory.ID, Description: dbCategory.Description, Name: dbCategory.Name}
@@ -75,7 +84,7 @@ func (repo *CategoryGormRepo) Find(req *domain.CategoryQuery) (*[]domain.Categor
 	}
 
 	if result := queryBuilder.Find(&categories); result.Error != nil {
-		return nil, result.Error
+		return nil, errors.New("unknown error")
 	}
 
 	return &categories, nil
