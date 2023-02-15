80e32c50fa65dc9801a7c1925a932287f59d19b4
nomorechokedboy <haiduongit@protonmail.com>
2023-02-15 11:36:27 +0700

    feat(api/product): add implementation for update, delete, find, find one method

--- apps/api/src/product/repository/gorm/gorm.go
+++ apps/api/src/product/repository/gorm/gorm.go
@@ -1,7 +1,10 @@
 package gorm
 
 import (
+	categoryGorm "api/src/category/repository/gorm"
 	inventory "api/src/inventory/domain"
+	"api/src/scopes"
+	"strings"
 
 	"api/src/product/domain"
 	"api/src/utils"
@@ -9,6 +12,7 @@ import (
 
 	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ProductGormRepo struct {
@@ -16,18 +20,28 @@ type ProductGormRepo struct {
 }
 
 func (r *ProductGormRepo) Insert(req *domain.ProductReq) (*domain.Product, error) {
+	categoryRepo := categoryGorm.CategoryGormRepo{DB: r.Conn}
+	category, err := categoryRepo.FindOne(utils.GetDataTypeAddress(int(req.CategoryId)))
+	if err != nil {
+		return nil, err
+	}
+
 	product := domain.Product{
 		CategoryID:  req.CategoryId,
 		Inventory:   inventory.Inventory{Quantity: req.Quantity},
+		Category:    *category,
 		Slug:        utils.Slug(req.Name),
 		Description: req.Description,
 		Name:        req.Name,
 		SKU:         req.SKU,
 		Price:       req.Price,
 	}
-	if result := r.Conn.Create(&product); result.Error != nil {
+	if result := r.Conn.Joins(clause.Associations).Create(&product); result.Error != nil {
 		if result.Error.(*pgconn.PgError).Code == "23505" {
 			return nil, errors.New("resource exist")
+		}
+		if result.Error.(*pgconn.PgError).Code == "23503" {
+			return nil, errors.New("resource conflict")
 		}
 
 		return nil, errors.New("unknown error")
@@ -37,17 +51,84 @@ func (r *ProductGormRepo) Insert(req *domain.ProductReq) (*domain.Product, error
 }
 
 func (r *ProductGormRepo) Delete(id uint) (*domain.Product, error) {
-	return nil, nil
+	deleteReq := domain.Product{ID: id}
+	result := r.Conn.Clauses(clause.Returning{}).Delete(&deleteReq)
+	if result.Error != nil {
+		return nil, errors.New("unknown error")
+	}
+	if result.RowsAffected == 0 {
+		return nil, errors.New("not found")
+	}
+
+	return &deleteReq, nil
 }
 
 func (r *ProductGormRepo) Update(id uint, req domain.ProductReq) (*domain.Product, error) {
-	return nil, nil
+	updateProduct, err := r.FindOne(id)
+	if err == nil && updateProduct == nil {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	updateProduct.CategoryID = req.CategoryId
+	updateProduct.Inventory.Quantity = req.Quantity
+	updateProduct.Name = req.Name
+	updateProduct.Description = req.Description
+	updateProduct.SKU = req.SKU
+	updateProduct.Price = req.Price
+	updateProduct.Slug = utils.Slug(req.Name)
+
+	result := r.Conn.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&updateProduct)
+	if result.Error != nil {
+		if result.Error.(*pgconn.PgError).Code == "23505" {
+			return nil, errors.New("resource exist")
+		}
+		if result.Error.(*pgconn.PgError).Code == "23503" {
+			return nil, errors.New("resource conflict")
+		}
+
+		return nil, errors.New("unknown error")
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, nil
+	}
+
+	return updateProduct, nil
 }
 
 func (r *ProductGormRepo) FindOne(id uint) (*domain.Product, error) {
-	return nil, nil
+	product := domain.Product{ID: id}
+
+	if result := r.Conn.Joins("Inventory").Joins("Category").First(&product); result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+
+		return nil, errors.New("unknown error")
+	}
+
+	return &product, nil
 }
 
 func (r *ProductGormRepo) Find(queries *domain.ProductQuery) ([]*domain.Product, error) {
-	return nil, nil
+	var products []*domain.Product
+	queryBuilder := r.Conn.Scopes(scopes.Pagination(&queries.BaseQuery))
+
+	if queries.CategoryId != nil {
+		queryBuilder.Where("category_id = ?", queries.CategoryId)
+	}
+
+	if queries.Q != nil {
+		queryBuilder.Where("name ILIKE ?", utils.EscapeLike("%", "%", strings.ToLower(*queries.Q))).Or("description ILIKE ?", utils.EscapeLike("%", "%", strings.ToLower(*queries.Q))).Or("sku ILIKE ?", utils.EscapeLike("%", "%", strings.ToLower(*queries.Q)))
+	}
+
+	if result := queryBuilder.Find(&products); result.Error != nil {
+		return nil, errors.New("unknown error")
+	}
+
+	return products, nil
 }
