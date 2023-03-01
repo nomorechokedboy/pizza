806bf7092b4867a1ad1191697bcc1edd2aeda69c
Dương <85113374+nomorechokedboy@users.noreply.github.com>
2023-03-01 11:20:52 +0700

    feat(api-blog): add minio (#32)

--- apps/api-blog/main.go
+++ apps/api-blog/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
 
 func main() {
+	// TODO: Refactor this minio connection code
+	ctx := context.Background()
+	endpoint := "localhost:9000"
+	accessKeyID := "admin"
+	secretAccessKey := "admin123"
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Change this code since it won't work in your local machine
+	bucketName := "general"
+	objectName := "todo-actix.zip"
+	filePath := "./tmp/todo-actix.zip"
+	contentType := "application/zip"
+
+	// Upload the zip file with FPutObject
+	info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
 	fmt.Println("Hello Cho vien")
 }
