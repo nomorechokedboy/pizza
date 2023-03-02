4498ed96cdd115f82c0146ae2a2a2e5dface5719
nomorechokedboy <haiduongit@protonmail.com>
2023-03-02 22:38:38 +0700

    feat(ui-vue): implement loading state for Button component

--- packages/ui-vue/components/Button/Button.stories.vue
+++ packages/ui-vue/components/Button/Button.stories.vue
@@ -6,43 +6,80 @@ import Button, { ButtonProps } from './Button.vue'
 const state = reactive<ButtonProps & { content: string; disabled: boolean }>({
 	disabled: false,
 	block: false,
+	loading: false,
 	content: 'Hello world',
 	size: 'sm',
-	radius: 'sm'
+	radius: 'sm',
+	loaderPosition: 'left'
 })
 </script>
 
 <template>
 	<Story>
-		<template #controls>
-			<HstText v-model="state.content" title="Content" />
-			<HstSelect
-				v-model="state.size"
-				title="Size"
-				:options="Size"
-			/>
-			<HstSelect
-				v-model="state.radius"
-				title="Radius"
-				:options="Size"
-			/>
-			<HstCheckbox
-				v-model="state.disabled"
-				title="Disabled"
-			/>
-			<HstCheckbox v-model="state.block" title="Block" />
-		</template>
-		<Variant auto-props-disabled>
+		<Variant title="Usage" auto-props-disabled>
+			<template #controls>
+				<HstText
+					v-model="state.content"
+					title="Content"
+				/>
+				<HstSelect
+					v-model="state.size"
+					title="Size"
+					:options="Size"
+				/>
+				<HstSelect
+					v-model="state.radius"
+					title="Radius"
+					:options="Size"
+				/>
+				<HstCheckbox
+					v-model="state.disabled"
+					title="Disabled"
+				/>
+				<HstCheckbox
+					v-model="state.loading"
+					title="Loading"
+				/>
+				<HstCheckbox
+					v-model="state.block"
+					title="Block"
+				/>
+			</template>
 			<div class="max-w-xs w-full">
 				<Button
 					:disabled="state.disabled"
 					:size="state.size"
 					:radius="state.radius"
 					:block="state.block"
+					:loading="state.loading"
 				>
 					{{ state.content }}
 				</Button>
 			</div>
+		</Variant>
+		<Variant title="Loading state" auto-props-disabled>
+			<template #controls>
+				<HstCheckbox
+					v-model="state.loading"
+					title="Loading"
+				/>
+				<HstSelect
+					v-model="state.loaderPosition"
+					title="Loader position"
+					:options="{
+						left: 'left',
+						right: 'right',
+						center: 'center'
+					}"
+				/>
+			</template>
+			<Button
+				:loading="state.loading"
+				:size="state.size"
+				:radius="state.radius"
+				:loader-position="state.loaderPosition"
+				>Loading...</Button
+			>
 		</Variant>
 	</Story>
 </template>
--- packages/ui-vue/components/Button/Button.vue
+++ packages/ui-vue/components/Button/Button.vue
@@ -1,56 +1,58 @@
 <script lang="ts" setup>
 import type { UISize } from '../common'
+import Loader, { LoaderProps } from '../Loader/Loader.vue'
+import LoadingOverlay from '../LoadingOverlay/LoadingOverlay.vue'
+import Overlay from '../Overlay/Overlay.vue'
 
 export interface ButtonProps {
 	size?: UISize
 	radius?: UISize
 	block?: boolean
 	loading?: boolean
+	loaderPosition?: 'left' | 'right' | 'center'
+	loaderProps?: LoaderProps
 }
 
 const {
-	radius = 'sm',
-	size = 'sm',
-	block = false,
-	loading = false
+	radius,
+	size,
+	loaderPosition,
+	block,
+	loading,
+	loaderProps: userLoaderProps
 } = defineProps<ButtonProps>()
+const loaderProps = userLoaderProps ?? { size: 'sm' }
 </script>
 
 <template>
 	<button
 		:data-block="block"
-		:data-size="size"
-		:data-radius="radius"
+		:data-size="size ?? 'sm'"
+		:data-radius="radius ?? 'sm'"
 		:data-loading="loading"
 		class="button"
 	>
-		<slot name="leftIcon" />
+		<Overlay v-if="loading" class="overide-overlay" />
+		<Loader
+			v-if="loading && loaderPosition === 'left'"
+			v-bind="loaderProps"
+		/>
+		<slot v-else name="leftIcon" />
+		<LoadingOverlay
+			v-if="loading && loaderPosition === 'center'"
+			:visible="loading && loaderPosition === 'center'"
+			:loader-props="loaderProps"
+		/>
 		<slot />
-		<slot name="rightIcon" />
+		<Loader
+			v-if="loading && loaderPosition === 'right'"
+			v-bind="loaderProps"
+		/>
+		<slot v-else name="rightIcon" />
 	</button>
 </template>
 
 <style lang="css" scoped>
-.button[data-radius='xs'] {
-	@apply rounded-sm;
-}
-
-.button[data-radius='sm'] {
-	@apply rounded;
-}
-
-.button[data-radius='md'] {
-	@apply rounded-lg;
-}
-
-.button[data-radius='lg'] {
-	@apply rounded-2xl;
-}
-
-.button[data-radius='xl'] {
-	@apply rounded-4xl;
-}
-
 .button[data-size='xs'] {
 	@apply px-3.5 py-1;
 }
@@ -62,18 +64,28 @@ const {
 .button[data-size='md'] {
 	@apply px-5 py-2;
 }
+
 .button[data-size='lg'] {
 	@apply px-6.5 py-3.5;
 }
+
 .button[data-size='xl'] {
 	@apply px-8 py-5;
 }
 
 .button {
-	@apply bg-sky-300 active:translate-y-px flex flex-row gap-4 items-center justify-center font-semibold overflow-hidden disabled:bg-slate-200 disabled:text-slate-400 disabled:translate-y-0;
+	@apply bg-sky-300 active:translate-y-px flex flex-row gap-4 items-center justify-center font-semibold overflow-hidden relative disabled:bg-slate-200 disabled:text-slate-400 disabled:translate-y-0;
 }
 
 .button[data-block='true'] {
 	@apply w-full;
+}
+
+.button[data-loading='true'] {
+	@apply pointer-events-none;
+}
+
+.overide-overlay {
+	@apply bg-black/20;
 }
 </style>
