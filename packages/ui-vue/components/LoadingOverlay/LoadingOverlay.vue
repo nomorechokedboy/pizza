2a973257dd421f0e7086f9b7248373c5fe4c6e06
nomorechokedboy <haiduongit@protonmail.com>
2023-03-02 22:37:56 +0700

    feat(ui-vue): add LoadingOverlay component

---
+++ packages/ui-vue/components/LoadingOverlay/LoadingOverlay.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Overlay, { OverlayProps } from '../Overlay/Overlay.vue'
+import Loader, { LoaderProps } from '../Loader/Loader.vue'
+
+export interface LoadingOverlayProps {
+	visible: boolean
+	loaderProps?: LoaderProps
+	overlayProps?: OverlayProps
+}
+
+const {
+	visible,
+	loaderProps: userLoaderProps,
+	overlayProps: userOverlayProps
+} = defineProps<LoadingOverlayProps>()
+const loaderProps = userLoaderProps ?? { size: 'md' }
+const overlayProps = userOverlayProps ?? { center: true }
+</script>
+
+<template>
+	<Overlay v-if="visible" v-bind="overlayProps">
+		<Loader v-bind="loaderProps" />
+	</Overlay>
+</template>
