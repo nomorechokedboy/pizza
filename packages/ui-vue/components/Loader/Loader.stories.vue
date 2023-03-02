518c1fbdae9bc1b399c7da90144ecd92fc7bc5c7
nomorechokedboy <haiduongit@protonmail.com>
2023-03-02 22:35:33 +0700

    feat(ui-vue): add Loader component

---
+++ packages/ui-vue/components/Loader/Loader.stories.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { Size } from '../common'
+import Loader, { LoaderProps } from './Loader.vue'
+
+const state = reactive<LoaderProps>({
+	size: 'sm'
+})
+</script>
+
+<template>
+	<Story>
+		<template #controls>
+			<HstSelect
+				v-model="state.size"
+				title="Size"
+				:options="Size"
+			/>
+		</template>
+		<Variant auto-props-disabled>
+			<Loader :size="state.size" />
+		</Variant>
+	</Story>
+</template>
