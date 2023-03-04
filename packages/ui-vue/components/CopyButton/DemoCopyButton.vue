659b28fce102c5fa39ac65a5938f69a36d9299ac
nomorechokedboy <haiduongit@protonmail.com>
2023-03-04 22:31:02 +0700

    feat(ui-vue): added CopyButton

---
+++ packages/ui-vue/components/CopyButton/DemoCopyButton.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import Button from '../Button/Button.vue'
+
+export interface DemoCopyButtonProps {
+	copied: boolean
+	copy: () => void
+}
+
+const { copied, copy } = defineProps<DemoCopyButtonProps>()
+</script>
+
+<template>
+	<Button :color="copied ? 'teal' : 'blue'" @click="copy">
+		{{ copied ? 'Copied url' : 'Copy url' }}
+	</Button>
+</template>
