4cd224cff4df0b9a09557f0bd8d8fe7e5a09a792
nomorechokedboy <haiduongit@protonmail.com>
2023-03-15 13:25:32 +0700

    feat(web-nuxt): integrate tauri mobile

--- apps/web-nuxt/nuxt.config.ts
+++ apps/web-nuxt/nuxt.config.ts
@@ -1,29 +1,35 @@
+import { NuxtConfig } from '@nuxt/schema'
 import defaultNuxtConfig from '../../nuxt.config'
+import { internalIpV4 } from 'internal-ip'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-	...defaultNuxtConfig,
-	css: ['ui-vue/dist/style.css'],
-	vite: {
-		clearScreen: false,
-		envPrefix: ['VITE_', 'TAURI_'],
-		build: {
-			target: 'es2022',
-			// don't minify for debug builds
-			minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-			// produce sourcemaps for debug builds
-			sourcemap: !!process.env.TAURI_DEBUG
+export default defineNuxtConfig(async () => {
+	const host = await internalIpV4()
+	const config: NuxtConfig = {
+		...defaultNuxtConfig,
+		css: ['ui-vue/dist/style.css'],
+		vite: {
+			clearScreen: false,
+			envPrefix: ['VITE_', 'TAURI_'],
+			build: {
+				target: 'es2022',
+				// don't minify for debug builds
+				minify: !process.env.TAURI_DEBUG
+					? 'esbuild'
+					: false,
+				// produce sourcemaps for debug builds
+				sourcemap: !!process.env.TAURI_DEBUG
+			},
+			server: {
+				strictPort: true,
+				hmr: {
+					protocol: 'ws',
+					host,
+					port: 5183
+				}
+			}
 		}
-		// server: {
-		// 	strictPort: true,
-		// 	hmr: {
-		// 		protocol: 'ws',
-		// 		host,
-		// 		port: 5183
-		// 	}
-		// }
-	},
-	devServer: {
-		host: '0.0.0.0'
 	}
+
+	return config
 })
