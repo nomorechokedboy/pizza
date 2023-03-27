857c42e2d9d5abbbb512d3817acb109d5d7a3d16
tringuyen276 <72869134+sekke276@users.noreply.github.com>
2023-03-27 09:38:08 -0700

    Feat/media (#45)
    * haven't write swagger for get media
    
    * refactor usecase add reset-password
    
    * merge
    
    * refactor usecase add reset-password
    
    * fix the get media api
    
    * merge
    
    * fix main
    
    * fix getmedia
    
    ---------
    
    Co-authored-by: sekke276 <tringuyen@gmail.com>

---
+++ apps/api-blog/api/handler/media.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"api-blog/api/config"
+	"context"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/minio/minio-go/v7"
+)
+
+type MediaHandler struct {
+	config      config.Config
+	minioClient minio.Client
+}
+
+func NewMediaHandler(config config.Config, mionioClient *minio.Client) *MediaHandler {
+	return &MediaHandler{
+		config:      config,
+		minioClient: *mionioClient,
+	}
+}
+
+// @PostImage godoc
+// @Summary Post to save image
+// @Tags Media
+// @ID	image
+// @Produce		json
+// @Accept	multipart/form-data
+// @Param image formData file true "upfile"
+// @Success 200
+// @Router /media/upload [post]
+func (handler *MediaHandler) PostImage(c *fiber.Ctx) error {
+	ctx := context.Background()
+	file, err := c.FormFile("image")
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	buffer, err := file.Open()
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	objectName := strings.ReplaceAll(file.Filename, " ", "")
+	fileBuffer := buffer
+	contentType := file.Header["Content-Type"][0]
+	fileSize := file.Size
+	_, err = handler.minioClient.PutObject(ctx, handler.config.Minio.BucketName, objectName, fileBuffer, fileSize, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(objectName)
+}
+
+// @GetMedia godoc
+// @Summary get Media
+// @Tags Media
+// @Accept json
+// @Param objectName path string true "imageName"
+// @Produce png
+// @Success 200
+// @Failure 404  "Cannot found the Image"
+// @Failure 500 "Cannot get Image"
+// @Router /media/{objectName} [get]
+func (handler *MediaHandler) GetMedia(c *fiber.Ctx) error {
+	ctx := context.Background()
+	objectName := c.Params("objectName")
+	if objectName == "" {
+		return fiber.NewError(fiber.ErrBadGateway.Code, "invalid object name")
+	}
+	_, err := handler.minioClient.StatObject(ctx, handler.config.Minio.BucketName, objectName, minio.StatObjectOptions{})
+	if err != nil {
+		return fiber.NewError(fiber.StatusNotFound, "Can not found Image")
+	}
+	newObject, err := handler.minioClient.GetObject(ctx, handler.config.Minio.BucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Can not get Image")
+	}
+	c.Set("Content-Type", "image/png")
+	return c.SendStream(newObject)
+}
