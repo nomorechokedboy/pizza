ae71223d195803ab6d096d6c651575186f6889d9
CPea <vienpro2506@gmail.com>
2023-03-28 18:51:27 +0700

    feat(api-blogs/post): expose `slug` and `published` field to post

--- apps/api-blog/api/gorm_repository/post.go
+++ apps/api-blog/api/gorm_repository/post.go
@@ -3,6 +3,8 @@ package gorm_repository
 import (
 	"api-blog/pkg/entities"
 	"api-blog/pkg/repository"
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -29,7 +31,7 @@ func (r *PostGormRepo) CreatePost(post *entities.Post) (uint, error) {
 func (r *PostGormRepo) GetPostBySlug(slug string) (*entities.Post, error) {
 	var post entities.Post
 
-	if err := r.db.Joins("JOIN slugs ON slugs.post_id = posts.id AND slugs.slug = ?", slug).First(&post).Error; err != nil {
+	if err := r.db.Joins("JOIN slugs ON slugs.post_id = posts.id AND slugs.slug = ? AND posts.deleted_at IS NULL", slug).First(&post).Error; err != nil {
 		return nil, err
 	}
 
@@ -39,7 +41,7 @@ func (r *PostGormRepo) GetPostBySlug(slug string) (*entities.Post, error) {
 func (r *PostGormRepo) GetAllPosts() ([]entities.Post, error) {
 	var posts []entities.Post
 
-	if err := r.db.Find(&posts).Error; err != nil {
+	if err := r.db.Find(&posts, "deleted_at IS NULL").Error; err != nil {
 		return nil, err
 	}
 
@@ -49,7 +51,7 @@ func (r *PostGormRepo) GetAllPosts() ([]entities.Post, error) {
 func (r *PostGormRepo) GetAllPostsByParentID(parentID uint) ([]entities.Post, error) {
 	var posts []entities.Post
 
-	if err := r.db.Find(&posts, "parent_id = ?", parentID).Error; err != nil {
+	if err := r.db.Find(&posts, "parent_id = ? AND deleted_at IS NULL", parentID).Error; err != nil {
 		return nil, err
 	}
 
@@ -59,22 +61,27 @@ func (r *PostGormRepo) GetAllPostsByParentID(parentID uint) ([]entities.Post, er
 func (r *PostGormRepo) GetAllPostsByUserID(userID uint) ([]entities.Post, error) {
 	var posts []entities.Post
 
-	if err := r.db.Find(&posts, "user_id = ?", userID).Error; err != nil {
+	if err := r.db.Find(&posts, "user_id = ? AND deleted_at IS NULL", userID).Error; err != nil {
 		return nil, err
 	}
+
 	return posts, nil
 }
 
 func (r *PostGormRepo) UpdatePost(post *entities.Post) error {
-	return r.db.Model(&post).Updates(&entities.Post{
-		Title:    post.Title,
-		ParentID: post.ParentID,
-		Content:  post.Content,
-	}).Error
+	return r.db.Model(&post).Where("deleted_at IS NULL").Updates(
+		&entities.Post{
+			Title:    post.Title,
+			ParentID: post.ParentID,
+			Slug:     post.Slug,
+			Content:  post.Content,
+		}).Updates(
+		map[string]interface{}{
+			"published":    post.Published,
+			"published_at": post.PublishedAt,
+		}).Error
 }
 
 func (r *PostGormRepo) DeletePost(id uint) error {
-	post := entities.Post{ID: id}
-
-	return r.db.Delete(&post).Error
+	return r.db.Model(&entities.Post{}).Where("id = ?", id).Update("deleted_at", time.Now()).Error
 }
--- apps/api-blog/pkg/usecase/post.go
+++ apps/api-blog/pkg/usecase/post.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"api-blog/pkg/entities"
 	"api-blog/pkg/repository"
+	"time"
 )
 
 type PostUsecase interface {
@@ -10,8 +11,8 @@ type PostUsecase interface {
 	GetAllPostsByUserID(userID uint) ([]entities.Post, error)
 	GetPostBySlug(slug string) (*entities.Post, error)
 	GetPostsByParentID(parentID uint) ([]entities.Post, error)
-	CreatePost(userID uint, body *entities.PostReq) (uint, error)
-	UpdatePost(id uint, body *entities.PostReq) error
+	CreatePost(userID uint, postSlug string, body *entities.PostReq) (uint, error)
+	UpdatePost(id uint, postSlug string, body *entities.PostReq) error
 	DeletePost(id uint) error
 }
 
@@ -35,13 +36,22 @@ func (usecase *postUsecase) GetPostBySlug(slug string) (*entities.Post, error) {
 	return usecase.repo.GetPostBySlug(slug)
 }
 
-func (usecase *postUsecase) CreatePost(userID uint, body *entities.PostReq) (uint, error) {
+func (usecase *postUsecase) CreatePost(userID uint, postSlug string, body *entities.PostReq) (uint, error) {
+	var publishedAt *time.Time = nil
+
+	if body.Published {
+		t := time.Now()
+		publishedAt = &t
+	}
+
 	post := &entities.Post{
+		UserID:      userID,
 		Title:       body.Title,
 		ParentID:    body.ParentID,
+		Slug:        postSlug,
 		Content:     body.Content,
-		PublishedAt: body.PublishedAt,
-		UserID:      userID,
+		Published:   body.Published,
+		PublishedAt: publishedAt,
 	}
 
 	return usecase.repo.CreatePost(post)
@@ -52,13 +62,22 @@ func (usecase *postUsecase) GetPostsByParentID(parentID uint) ([]entities.Post, 
 	return usecase.repo.GetAllPostsByParentID(parentID)
 }
 
-func (usecase *postUsecase) UpdatePost(id uint, body *entities.PostReq) error {
+func (usecase *postUsecase) UpdatePost(id uint, postSlug string, body *entities.PostReq) error {
+	var publishedAt *time.Time = nil
+
+	if body.Published {
+		t := time.Now()
+		publishedAt = &t
+	}
+
 	post := &entities.Post{
 		ID:          id,
 		Title:       body.Title,
 		ParentID:    body.ParentID,
+		Slug:        postSlug,
 		Content:     body.Content,
-		PublishedAt: body.PublishedAt,
+		Published:   body.Published,
+		PublishedAt: publishedAt,
 	}
 
 	return usecase.repo.UpdatePost(post)
