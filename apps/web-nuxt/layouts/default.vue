883e54ee06968c2ff7a6a4c460b3e9d1d7e18c69
nomorechokedboy <haiduongit@protonmail.com>
2023-04-10 23:01:28 +0700

    refactor(web-nuxt): refactor layouts

--- apps/web-nuxt/layouts/default.vue
+++ apps/web-nuxt/layouts/default.vue
@@ -11,7 +11,9 @@ useHead({
 <template>
 	<div class="flex flex-col min-h-screen relative">
 		<header class="sticky top-0 z-20">
-			<Header />
+			<slot name="header">
+				<Header />
+			</slot>
 		</header>
 		<div
 			class="fixed flex flex-col gap-5 top-20 right-0 z-40 max-w-xs lg:mr-5"
@@ -23,6 +25,12 @@ useHead({
 			/>
 		</div>
 		<slot />
-		<footer class="min-h-[233px] bg-neutral-200">Footer</footer>
+		<footer>
+			<slot name="footer">
+				<div class="min-h-[233px] bg-neutral-200">
+					Footer
+				</div>
+			</slot>
+		</footer>
 	</div>
 </template>
--- apps/web-nuxt/layouts/new.vue
+++ apps/web-nuxt/layouts/new.vue
@@ -1,16 +1,10 @@
-<script setup lang="ts">
-useHead({
-	bodyAttrs: {
-		class: 'bg-neutral-100'
-	}
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<div class="flex flex-col h-screen">
-		<header>
-			<Header />
-		</header>
+	<NuxtLayout name="default">
 		<slot />
-	</div>
+		<template #footer>
+			<div></div>
+		</template>
+	</NuxtLayout>
 </template>
