8b3833738fefb67e9013f26c8be06be85b377fd0
nomorechokedboy <haiduongit@protonmail.com>
2023-04-12 14:26:02 +0700

    feat(web-nuxt): remove sidebar color

--- apps/web-nuxt/components/LeftSidebar.vue
+++ apps/web-nuxt/components/LeftSidebar.vue
@@ -1,11 +1,13 @@
 <template>
 	<aside
-		class="absolute hidden top-0 left-0 w-full h-screen bg-black/50 z-50 md:!relative md:max-w-[212px] md:z-10 md:block"
+		class="absolute hidden top-0 left-0 w-full h-screen bg-black/50 z-50 md:!relative md:max-w-[212px] md:z-10 md:block md:bg-transparent"
 	>
-		<div class="w-4/5 h-full bg-green-500 flex flex-col md:w-full">
-			<header class="bg-blue-300 h-14"></header>
-			<nav class="h-full bg-lime-300 p-2"></nav>
-			<footer class="bg-indigo-500 h-14"></footer>
+		<div
+			class="w-4/5 h-full bg-transparent flex flex-col md:w-full"
+		>
+			<header class="bg-transparent h-14"></header>
+			<nav class="h-full bg-transparent p-2"></nav>
+			<footer class="bg-transparent-500 h-14"></footer>
 		</div>
 	</aside>
 </template>
