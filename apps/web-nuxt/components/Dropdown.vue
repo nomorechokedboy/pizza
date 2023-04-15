940b4c69f7c74a697dac34b0df0e07050f1494e7
nomorechokedboy <haiduongit@protonmail.com>
2023-04-15 10:30:55 +0700

    refactor(web-nuxt): refactored Dropdown component

--- apps/web-nuxt/components/Dropdown.vue
+++ apps/web-nuxt/components/Dropdown.vue
@@ -1,15 +1,6 @@
 <script lang="ts" setup>
 export interface DropdownProps {
-	user: {
-		name: string
-		username: string
-	}
 	open: boolean
-}
-
-async function logout() {
-	removeToken()
-	navigateTo('/login')
 }
 
 const { open } = defineProps<DropdownProps>()
@@ -20,58 +11,11 @@ const { open } = defineProps<DropdownProps>()
 		class="dropdown absolute mt-2 px-2 py-2 z-10 text-md bg-white divide-y divide-gray-100 rounded-lg shadow w-[15.625rem] dark:bg-gray-700"
 		:data-biding="open"
 	>
-		<div
-			class="pb-2 mb-2 hover:underline hover:rounded-lg px-4 py-3 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-		>
-			<a href="#">
-				<div>
-					<span class="fw-medium block">
-						{{ user.name }}
-					</span>
-					<small class="text-gray-400"
-						>@{{ user.username }}
-					</small>
-				</div>
-			</a>
-		</div>
-		<ul
-			class="py-2 text-gray-700 dark:text-gray-200"
-			aria-labelledby="dropdownDefaultButton"
-		>
-			<!-- <li>
-                <a href="#"
-                    class="block px-4 py-2 hover:underline hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-            </li> -->
-			<li>
-				<NuxtLink
-					to="/new"
-					class="block px-4 py-2 hover:underline hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-				>
-					Create Post</NuxtLink
-				>
-			</li>
-			<!-- <li>
-                <a href="#"
-                    class="block px-4 py-2 hover:underline hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reading
-                    list</a>
-            </li>
-            <li>
-                <a href="#"
-                    class="block px-4 py-2 hover:underline hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-            </li> -->
-		</ul>
-		<div class="pt-2 dark:text-gray-200">
-			<div
-				class="cursor-pointer px-4 py-2 hover:underline hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-				@click="logout"
-			>
-				Sign out
-			</div>
-		</div>
+		<slot />
 	</div>
 </template>
 
-<style lang="css" scoped>
+<style lang="postcss" scoped>
 .dropdown {
 	@apply -right-2;
 }
