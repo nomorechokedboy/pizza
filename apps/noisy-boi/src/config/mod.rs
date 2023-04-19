edd61d557855e890e01a400b95e101d0eccea119
nomorechokedboy <haiduongit@protonmail.com>
2023-04-20 00:11:15 +0700

    chore(noisy-boi): initialize noisy-boi

---
+++ apps/noisy-boi/src/config/mod.rs
@@ -0,0 +1,62 @@
+use std::net::Ipv4Addr;
+
+use config::{Environment, File, FileFormat};
+use serde::Deserialize;
+
+const CONFIG_PATH: &str = "./src/config/config";
+
+#[derive(Debug, Deserialize)]
+pub struct Server {
+    pub port: u16,
+    pub host: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Log {
+    pub level: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Database {
+    pub host: String,
+    pub name: String,
+    pub user: String,
+    pub password: String,
+    pub port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AppSettings {
+    pub database: Database,
+    pub server: Server,
+    pub log: Log,
+}
+
+impl AppSettings {
+    pub fn new() -> Result<Self, config::ConfigError> {
+        let cfg_builder = config::Config::builder()
+            .add_source(File::new(CONFIG_PATH, FileFormat::Json))
+            .add_source(Environment::default().separator("_"));
+        cfg_builder.build()?.try_deserialize::<Self>()
+    }
+
+    pub fn server_url(&self) -> String {
+        let Server { port, host } = self.server;
+        let host = match host {
+            true => Ipv4Addr::UNSPECIFIED,
+            false => Ipv4Addr::LOCALHOST,
+        };
+        format!("{host}:{port}")
+    }
+
+    pub fn db_url(&self) -> String {
+        let Database {
+            host,
+            name,
+            user,
+            password,
+            port,
+        } = &self.database;
+        format!("postgres://{user}:{password}@{host}:{port}/{name}")
+    }
+}
---
+++ apps/noisy-boi/src/main.rs
@@ -0,0 +1,128 @@
+mod config;
+use crate::config::AppSettings;
+use actix_web::{
+    get,
+    middleware::Logger,
+    web::{self, Redirect},
+    App, HttpServer, Responder, Result, Scope,
+};
+use serde::Serialize;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::{
+    sync::Arc,
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+};
+use tracing::info;
+use utoipa::{
+    openapi::{
+        security::{ApiKey, ApiKeyValue, SecurityScheme},
+        Server,
+    },
+    Modify, OpenApi,
+};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi(
+    info(description = "My Api description"), 
+    paths(
+        health_check
+    ),
+    modifiers(&SecurityAddon),
+    components(
+        schemas(HealthCheckResponse)
+    )
+)]
+struct ApiDoc;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let components = openapi.components.as_mut().unwrap();
+        openapi.servers = Some(vec![Server::new("/api/v1")]);
+        components.add_security_scheme(
+            "api_key",
+            SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("todo_apikey"))),
+        )
+    }
+}
+
+#[get("/")]
+pub async fn index() -> impl Responder {
+    Redirect::to("/docs/")
+}
+
+#[derive(utoipa::ToSchema, Serialize)]
+struct HealthCheckResponse {
+    message: String,
+    uptime: u64,
+    timestamp: u128,
+}
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "OK", body = HealthCheckResponse),
+    ),
+    tag = "HealthCheck",
+    /* security(
+        ("api_key" = [])
+    ), */
+)]
+#[get("/healthz")]
+pub async fn health_check(app_state: web::Data<AppState>) -> Result<impl Responder> {
+    let elapsed_time = app_state.start_time.elapsed();
+    let uptime = Duration::from_secs_f64(elapsed_time.as_secs_f64()).as_secs();
+    let now = SystemTime::now();
+    let since_epoch = now.duration_since(UNIX_EPOCH).unwrap();
+    let timestamp = since_epoch.as_millis();
+    let res = HealthCheckResponse {
+        message: String::from("Not dead yet"),
+        uptime,
+        timestamp,
+    };
+    Ok(web::Json(res))
+}
+
+pub struct AppState {
+    start_time: Instant,
+    pool: Arc<Pool<Postgres>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt().pretty().init();
+    let settings = AppSettings::new()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+    let server_url = settings.server_url();
+    let db_url = settings.db_url();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::TimedOut, e.to_string()))?;
+    let pool = Arc::new(pool);
+
+    let openapi = ApiDoc::openapi();
+    let start_time = Instant::now();
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                start_time,
+                pool: pool.clone(),
+            }))
+            .wrap(Logger::default())
+            .service(Scope::new("/api/v1").service(health_check))
+            .service(index)
+            .service(SwaggerUi::new("/docs/{_:.*}").url("/api-docs/openapi.json", openapi.clone()))
+    });
+
+    info!("Starting server at {server_url}");
+    info!("Api documentation is at {server_url}/docs/");
+    server.bind(server_url)?.run().await?;
+
+    Ok(())
+}
