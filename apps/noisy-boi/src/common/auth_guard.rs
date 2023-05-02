2da8bb22d7558d40b5b2dd674118cc6215a4704f
nomorechokedboy <haiduongit@protonmail.com>
2023-05-02 23:01:51 +0700

    refactor(noisy-boi): using env instead of hard-code token secret

--- apps/noisy-boi/src/common/auth_guard.rs
+++ apps/noisy-boi/src/common/auth_guard.rs
@@ -1,8 +1,13 @@
-use actix_web::{error::ErrorUnauthorized, http, FromRequest};
+use actix_web::{
+    error::{ErrorInternalServerError, ErrorUnauthorized},
+    http, web, FromRequest,
+};
 use jsonwebtoken::{decode, DecodingKey, Validation};
 use serde::{Deserialize, Serialize};
 use std::future::{ready, Ready};
 use tracing::error;
+
+use super::app_state::AppState;
 
 #[derive(Debug)]
 pub struct AuthGuard {
@@ -20,42 +25,49 @@ impl FromRequest for AuthGuard {
     type Future = Ready<Result<Self, Self::Error>>;
 
     fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
-        let res = match req.cookie("token").map(|c| c.value().to_string()) {
-            Some(token) => Ok(token),
-            None => req
-                .headers()
-                .get(http::header::AUTHORIZATION)
-                .ok_or(ErrorUnauthorized("Missing token"))
-                .and_then(|auth_value| {
-                    auth_value
-                        .to_str()
-                        .map_err(|e| {
-                            error!("{e}");
-                            ErrorUnauthorized("Missing token")
-                        })
-                        .and_then(|bearer| {
-                            bearer
-                                .split(" ")
-                                .last()
-                                .ok_or(ErrorUnauthorized("Missing token"))
-                                .map(|token| token.to_string())
-                        })
-                }),
-        }
-        .map(|token| {
-            decode::<TokenClaims>(
-                &token,
-                &DecodingKey::from_secret("token-secret".as_ref()),
-                &Validation::default(),
-            )
-            .map_err(|e| {
-                error!("Decode error: {e}");
-                ErrorUnauthorized("Invalid token")
-            })
-            .and_then(|decoded| Ok(decoded.claims))
-        })
-        .and_then(|claims| claims.map(|TokenClaims { sub: user_id, .. }| Self { user_id }));
+        let app_state = req.app_data::<web::Data<AppState>>();
+        match app_state {
+            None => ready(Err(ErrorInternalServerError("Internal error"))),
+            Some(app_state) => {
+                let settings = &app_state.settings;
+                let res = match req.cookie("token").map(|c| c.value().to_string()) {
+                    Some(token) => Ok(token),
+                    None => req
+                        .headers()
+                        .get(http::header::AUTHORIZATION)
+                        .ok_or(ErrorUnauthorized("Missing token"))
+                        .and_then(|auth_value| {
+                            auth_value
+                                .to_str()
+                                .map_err(|e| {
+                                    error!("{e}");
+                                    ErrorUnauthorized("Missing token")
+                                })
+                                .and_then(|bearer| {
+                                    bearer
+                                        .split(" ")
+                                        .last()
+                                        .ok_or(ErrorUnauthorized("Missing token"))
+                                        .map(|token| token.to_string())
+                                })
+                        }),
+                }
+                .map(|token| {
+                    decode::<TokenClaims>(
+                        &token,
+                        &DecodingKey::from_secret(&settings.server.token_secret.as_ref()),
+                        &Validation::default(),
+                    )
+                    .map_err(|e| {
+                        error!("Decode error: {e}");
+                        ErrorUnauthorized("Invalid token")
+                    })
+                    .and_then(|decoded| Ok(decoded.claims))
+                })
+                .and_then(|claims| claims.map(|TokenClaims { sub: user_id, .. }| Self { user_id }));
 
-        ready(res)
+                ready(res)
+            }
+        }
     }
 }
