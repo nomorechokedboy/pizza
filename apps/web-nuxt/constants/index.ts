982305dbbe9408147bd2558fcec3d4b00ffe51e9
nomorechokedboy <haiduongit@protonmail.com>
2023-05-03 12:04:52 +0700

    fix(web-nuxt): fix baseUrl

--- apps/web-nuxt/constants/index.ts
+++ apps/web-nuxt/constants/index.ts
@@ -1,4 +1,3 @@
-// export const baseURL = 'https://api-blog-dev-nomorechokedboy.cloud.okteto.net'
-export const baseURL = 'http://localhost:8080'
+export const baseURL = 'https://api-blog-dev-nomorechokedboy.cloud.okteto.net'
 export const baseMedia = `${baseURL}/api/v1/media`
 export const dicebearMedia = 'https://api.dicebear.com/6.x/initials/svg?seed='
