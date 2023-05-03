2e8b525385ade31cfae7b688b91f19cf0f06c1d3
nomorechokedboy <haiduongit@protonmail.com>
2023-05-03 08:23:02 +0700

    feat(web-nuxt): removed withCredential and updated media url

--- apps/web-nuxt/constants/index.ts
+++ apps/web-nuxt/constants/index.ts
@@ -1,3 +1,4 @@
-export const baseURL = 'https://api-blog-dev-nomorechokedboy.cloud.okteto.net'
-export const baseMedia = `${baseURL}/media`
+// export const baseURL = 'https://api-blog-dev-nomorechokedboy.cloud.okteto.net'
+export const baseURL = 'http://localhost:8080'
+export const baseMedia = `${baseURL}/api/v1/media`
 export const dicebearMedia = 'https://api.dicebear.com/6.x/initials/svg?seed='
